c656986d8f057b3381852ddb00c54274cc0bd36d
James Bridges <masterbridges@gmail.com>
2020-01-04 15:13:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,11 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :brand, :color, :material, :condition
+  attr_accessor :color, :material, :condition, :size
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
 
   def cobble
     puts "Your shoe is as good as new!"
